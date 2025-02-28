42ba6b21b39a57e0fee3c424d0b111cb609b294b
Marek Bykowski <marek.bykowski@gmail.com>
2025-02-28 13:00:30 +0000

    Add ability for arm ds to connect to the FVP A55 running Zephyr
    Signed-off-by: Marek Bykowski <marek.bykowski@gmail.com>

--- run_hello_fvp_cortex-a.sh
+++ run_hello_fvp_cortex-a.sh
@@ -1,6 +1,9 @@
 export ARMFVP_BIN_PATH=~/Downloads/Base_RevC_AEM_Fast_Models_11.26/Base_RevC_AEMvA_pkg/models/Linux64_GCC-9.3
 export ZEPHYR_TOOLCHAIN_VARIANT=zephyr                                          
 
+# If you want to connect with arm-ds enable the line below
+# The fvp_base_revc_2xaemv8a would halt until the debugger connects
+# then execute 'cont' on the debugger to let the app run.
 export ARMFVP_EXTRA_FLAGS="--iris-server --iris-port 7100"
 
 west build -p always -b fvp_base_revc_2xaemv8a samples/synchronization          
